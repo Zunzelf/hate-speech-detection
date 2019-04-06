8fb352fd4f653e17837c0b506d606ee02c532eb3
fikrihasani <mfikrihasani@gmail.com>
2019-04-06 14:37:12 +0700

    Still error in predict
    Already in flask mode

--- app.py
+++ app.py
@@ -1,41 +1,51 @@
-from utils.feature_extraction import WordEmbed as w2v
 from classifier import nn
+from classifier.nn import Driver
 import numpy as np
-from utils.dataset import Data
+# from utils.dataset import Data
 from keras.utils import np_utils
 import os
-from utils import preprocess as prepro
-import pickle as pkl
 
-class driver():
-    def load_model(self, path):
-        with open(path, 'rb') as file:
-            self.model = pkl.load(file)
 
-    def load_word_model(self, path):
-        with open(path, 'rb') as file:
-            self.word_model = pkl.load(file)
+from flask import Flask, request, render_template
 
-    def predict(self, inp):
-        tesx = inp
-        tesx = prepro.clean(tesx)[:20]
-        test = w2v().sen2vec_2(tesx, self.word_model)
-        res = self.model.predict_classes(test)
-        return res
-    
-    def train_model(self, x, y):
-        pass
-        
+app = Flask(__name__)
+
+clsfr = Driver()
+# load classifier model
+clsfr.load_model('models/beta-3.mdl')
+# load classifier model
+clsfr.load_word_model('models/sen2vec.mdl')
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/classify", methods=["GET","POST"])
+def classify():
+    pred = text = ''
+    check = True
+    if request.method == "POST":
+        txt = request.form["inference"]
+        print("input: ",txt)
+        print("type: ",type(txt))
+        pred = clsfr.predict(txt)
+        if not pred:
+            check = False
+    return render_template("classify.html",txt=pred,check=check)
+
 if __name__ == '__main__':
-    dats = Data()
-    data_root = os.path.join('utils')
-    data_path = os.path.join(data_root, 'token-checked.bin')
 
-    with open(data_path, 'rb') as file:
-        datas = pkl.load(file)
-    dats.load_data('utils/labeled_data.csv')
-    word_model = w2v().load_model(path = 'utils/sen2vec.mdl')
-    pretrained_weights = word_model.wv.syn0
+    app.run()
+    # for training data: 
+    # dats = Data()
+    # data_root = os.path.join('utils')
+    # data_path = os.path.join(data_root, 'token-checked.bin')
+
+    # with open(data_path, 'rb') as file:
+    #     datas = pkl.load(file)
+    # dats.load_data('utils/labeled_data.csv')
+    # word_model = w2v().load_model(path = 'utils/sen2vec.mdl')
+    # pretrained_weights = word_model.wv.syn0
     
     # vocab_size, emdedding_size = pretrained_weights.shape
     # targets = dats.y
@@ -67,23 +77,25 @@ if __name__ == '__main__':
     # # inp = np.array([docs[0]])
     # # print(model.predict_classes(inp, batch_size = 1, verbose = 1))
 
-    from tqdm import tqdm
-    clsfr = driver()
-    # load classifier model
-    clsfr.load_model('beta-3.mdl')
-    # load classifier model
-    clsfr.load_word_model('utils/sen2vec.mdl')
+    # from tqdm import tqdm
+    # clsfr = driver()
+    # # load classifier model
+    # clsfr.load_model('beta-3.mdl')
+    # # load classifier model
+    # clsfr.load_word_model('utils/sen2vec.mdl')
+    # txt = ''
+    # pred = clsfr.predict(txt)
 
-    cnt = 0
-    for x,i in tqdm(enumerate(dats.x[:20])):
-        pnt = 221
-        txt = dats.x[pnt]
-        # to predict text ---> text, not list of strings
-        pred = clsfr.predict(txt)
-        # just for checking accuracy
-        orig = dats.y[pnt]
-        if pred[0] == orig:
-            cnt += 1
-    print('>>>>>', cnt)
+    # cnt = 0
+    # for x,i in tqdm(enumerate(dats.x[:20])):
+    #     pnt = 221
+    #     txt = dats.x[pnt]
+    #     # to predict text ---> text, not list of strings
+    #     pred = clsfr.predict(txt)
+    #     # just for checking accuracy
+    #     orig = dats.y[pnt]
+    #     if pred[0] == orig:
+    #         cnt += 1
+    # print('>>>>>', cnt)
         
 
--- classifier/nn.py
+++ classifier/nn.py
@@ -1,5 +1,9 @@
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Activation, Dropout, Embedding, Bidirectional
+from utils.dataset import Data
+import pickle as pkl
+from utils import preprocess as prepro
+from utils.feature_extraction import WordEmbed as w2v
 
 from keras.optimizers import RMSprop
 #using keras
@@ -13,12 +17,30 @@ class BiLSTM():
 		model.compile('adam', 'categorical_crossentropy', metrics=['accuracy'])
 		return model
 
-class LSTM():
-	def create_model(self, num_words, max_words = 20, hidden_units = 150):
-		model = Sequential()
-		model.add(Embedding(num_words, 100, input_length = max_words, trainable = False))
-		model.add(Bidirectional(LSTM(hidden_units)))
-		model.add(Dropout(0.5))
-		model.add(Dense(3, activation='softmax'))
-		model.compile('adam', 'categorical_crossentropy', metrics=['accuracy'])
-		return model
+# class LSTM():
+# 	def create_model(self, num_words, max_words = 20, hidden_units = 150):
+# 		model = Sequential()
+# 		model.add(Embedding(num_words, 100, input_length = max_words, trainable = False))
+# 		model.add(Bidirectional(LSTM(hidden_units)))
+# 		model.add(Dropout(0.5))
+# 		model.add(Dense(3, activation='softmax'))
+# 		model.compile('adam', 'categorical_crossentropy', metrics=['accuracy'])
+# 		return model
+class Driver():
+    def load_model(self, path):
+        with open(path, 'rb') as file:
+            self.model = pkl.load(file)
+
+    def load_word_model(self, path):
+        with open(path, 'rb') as file:
+            self.word_model = pkl.load(file)
+
+    def predict(self, inp):
+        tesx = inp
+        tesx = prepro.clean(tesx)[:20]
+        test = w2v().sen2vec_2(tesx, self.word_model)
+        res = self.model.predict_classes(test)
+        return res
+    
+    def train_model(self, x, y):
+        pass
