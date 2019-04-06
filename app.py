c7214d3ad431761f57b2939a43915493e0e3a80a
zunzelf <teza.nugroho@yahoo.co.id>
2019-04-06 13:05:01 +0700

    added pipeline to API

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from utils.feature_extraction import WordEmbed as w2v
-from classifier import nn
+import os, sys
+stderr = sys.stderr
+sys.stderr = open(os.devnull, 'w')
+from utils.feature_extraction import WordEmbed
+from utils.feature_extraction import word2idx, idx2word    
+from utils.dataset import Data, tokenize_doc, tokenize_sen
+from classifier.nn import BiLSTM
+from tqdm import tqdm
 import numpy as np
 from utils.dataset import Data
 from keras.utils import np_utils
-import os
 from utils import preprocess as prepro
 import pickle as pkl
+import multiprocessing as mp
+from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split as tts
+sys.stderr = stderr
+dats = Data()
 
 class driver():
     def load_model(self, path):
@@ -19,71 +29,126 @@ class driver():
     def predict(self, inp):
         tesx = inp
         tesx = prepro.clean(tesx)[:20]
-        test = w2v().sen2vec_2(tesx, self.word_model)
+        test = WordEmbed().sen2vec_2(tesx, self.word_model)
         res = self.model.predict_classes(test)
-        return res
+        return res[0]
+
+    def train_model(self, X, Y, X_test = None, Y_test = None, save_path = 'model_classifier.mdl', epochs = 100):        
+        pretrained_weights = self.word_model.wv.syn0
+        datas = X
+        targets = Y
+
+        vocab_size, emdedding_size = pretrained_weights.shape
+
+        num_target = len(set(targets))
+        targets = np_utils.to_categorical(targets, num_classes = num_target)
+        test_targets = np_utils.to_categorical(Y_test, num_classes = num_target)
+        datas = [x[:20] for x in datas]
+        test_data = [x[:20] for x in X_test]
+        train_x = np.zeros([len(datas), 20], dtype=np.int32)
+        train_y = np.zeros([len(datas), num_target], dtype=np.int32)
+        test_x = np.zeros([len(X_test), 20], dtype=np.int32)
+        test_y = np.zeros([len(Y_test), num_target], dtype=np.int32)
+        
+        for i, data in enumerate(datas):
+            for t, word in enumerate(data[:-1]):
+                train_x[i, t] = word2idx(word, self.word_model)
+            for x in range(num_target):
+                train_y[i, x] = targets[i, x]     
+
+        for i, data in enumerate(test_data):
+            for t, word in enumerate(data[:-1]):
+                test_x[i, t] = word2idx(word, self.word_model)
+            for x in range(num_target):
+                test_y[i, x] = test_targets[i, x]
+
+        self.model = BiLSTM.create_model(vocab_size, 20, hidden_units = 50)
+        self.model.fit(train_x, train_y, batch_size = 64, epochs = epochs, verbose=1, validation_data = [test_x, test_y])
+
+        with open(save_path, 'wb') as file:
+            pkl.dump(self.model, file)
     
-    def train_model(self, x, y):
-        pass
+    def train_word_model(self, sentences, save_path = 'model_word_vector.mdl', ndim = 100, window_size = 10, min_count = 2):
+        # the format for input -> [['word-1-sentence-1',..,'word-n-sentence-1'],..,['word-1-sentence-n',..,'word-n-sentence-n']]
+        datas = sentences
+        datas.extend([['$$'], ['$$']]) # 'padding' to handle <unknown> vocab
+        w2v = WordEmbed()
+        self.word_model = w2v.create_model(datas, size = ndim, window = window_size, min_count = min_count)
+        w2v.save_model(self.word_model, path = save_path)
+
+    def clean_dataset(self, dat, save_path = 'token-checked-2.bin', spell_check = True):
+        print("Preparing Parallelism...", end = '')
+        pool = mp.Pool(int(mp.cpu_count()))
+        dats = dat.x
+        print("complete! cpu ready : %i cpus" % int(mp.cpu_count()))
+
+        print("tokenize docs...")
+        tkn = list(tqdm(pool.imap(tokenize_sen, dats), total = len(dats)))
+        pool.close
+        print("tokenize docs...complete!")
+        
+        if spell_check:
+            print("spell checking tokens...")
+            pool = mp.Pool(int(mp.cpu_count()))    
+            res = list(tqdm(pool.imap(prepro.spell_check, tkn), total = len(tkn)))
+            pool.close
+            print("spell checking tokens...complete!")
+        elif not spell_check:
+            res = tkn
+
+        with open(save_path, 'wb') as file:
+            print('saving tokens...')
+            pkl.dump(res, file)
+        
+        return res
+
+    def generate_models(self, data_path, save_path, w2v_path = None, test = 0.3, spell_check = True, evaluate = True, epochs = 100):
+        dat = Data()
+        print("loading file...")
+        dat.load_data(data_path)
+        print("loading file...complete!")
+        cleaned_data = self.clean_dataset(dat, spell_check = spell_check)
+        train_x, test_x, train_y, test_y = tts(cleaned_data, dat.y, test_size = test)
+        
+        if w2v_path == None:
+            self.train_word_model(cleaned_data, save_path = 'generated_word_model.mdl')
+            print('Processed : %i vocabs'% len(self.word_model.wv.vocab))
+        
+        # freed some memory
+        del cleaned_data
+        del dat
+
+        self.train_model(train_x, train_y, test_x, test_y, save_path = save_path, epochs = epochs)
+        if evaluate:
+            self.evaluate(test_x, test_y)
+
+    def evaluate(self, test_x, test_y):
+            preds = []
+            orig = []
+            txts = []
+            for x in tqdm(range(len(test_x))):
+                txts = " ".join(test_x[x])
+                preds.append(self.predict(txts))
+                # just for checking accuracy
+                orig.append(test_y[x])
+                
+            score = f1_score(orig, preds, average = 'micro')   
+            print(score)
+
+        
+        
+
         
 if __name__ == '__main__':
-    dats = Data()
-    data_root = os.path.join('utils')
-    data_path = os.path.join(data_root, 'token-checked.bin')
-
-    with open(data_path, 'rb') as file:
-        datas = pkl.load(file)
-    dats.load_data('utils/labeled_data.csv')
-    word_model = w2v().load_model(path = 'utils/sen2vec.mdl')
-    pretrained_weights = word_model.wv.syn0
     
-    # vocab_size, emdedding_size = pretrained_weights.shape
-    # targets = dats.y
-
-    # num_target = len(set(targets))
-    # targets = np_utils.to_categorical(targets, num_classes = num_target)
-
-    # def idx2word(idx):
-    #     return word_model.wv.index2word[idx]
-    
-    # datas = [x[:20] for x in datas]
-    # train_x = np.zeros([len(datas), 20], dtype=np.int32)
-    # train_y = np.zeros([len(datas), num_target], dtype=np.int32)
-    # for i, datas in enumerate(datas):
-    #     for t, word in enumerate(datas[:-1]):
-    #         train_x[i, t] = word2idx(word)
-    #     train_y[i, 0] = targets[i, 0]
-    #     train_y[i, 1] = targets[i, 1]
-    #     train_y[i, 2] = targets[i, 2]
-    # print('train_x shape:', train_x.shape)
-    # print('train_y shape:', train_y.shape)
-
-
-    # model = nn.BiLSTM().create_model(vocab_size, 20, hidden_units = 50)
-    # model.fit(train_x, train_y, batch_size = 64, epochs=100, verbose=1)
-    # import pickle as pkl
-    # with open("beta-3.mdl", 'wb') as file:
-    #     pkl.dump(model, file)
-    # # inp = np.array([docs[0]])
-    # # print(model.predict_classes(inp, batch_size = 1, verbose = 1))
-
-    from tqdm import tqdm
     clsfr = driver()
-    # load classifier model
-    clsfr.load_model('beta-3.mdl')
-    # load classifier model
-    clsfr.load_word_model('utils/sen2vec.mdl')
+    # # load classifier model
+    # clsfr.load_word_model('utils/sen2vec.mdl')
 
-    cnt = 0
-    for x,i in tqdm(enumerate(dats.x[:20])):
-        pnt = 221
-        txt = dats.x[pnt]
-        # to predict text ---> text, not list of strings
-        pred = clsfr.predict(txt)
-        # just for checking accuracy
-        orig = dats.y[pnt]
-        if pred[0] == orig:
-            cnt += 1
-    print('>>>>>', cnt)
-        
+    # # load classifier model
+    # clsfr.load_model('beta-3.mdl')
+    # # clsfr.load_model('model_classifier.mdl')
+
+    dts = clsfr.generate_models(data_path = 'utils/labeled_data.csv', save_path = 'generated_classifier_model.mdl') 
+    print(dts)   
 
--- classifier/nn.py
+++ classifier/nn.py
@@ -1,24 +1,29 @@
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Activation, Dropout, Embedding, Bidirectional
-
 from keras.optimizers import RMSprop
 #using keras
+
 class BiLSTM():
-	def create_model(self, num_words, max_words = 20, hidden_units = 150):
+	def create_model(self, num_words, n_dims = 100, max_words = 20, hidden_units = 150, vectors = None):
 		model = Sequential()
-		model.add(Embedding(num_words, 100, input_length = max_words, trainable = False))
+		model.add(Embedding(num_words, n_dims, input_length = max_words, trainable = False))
 		model.add(Bidirectional(LSTM(hidden_units)))
 		model.add(Dropout(0.5))
 		model.add(Dense(3, activation='softmax'))
 		model.compile('adam', 'categorical_crossentropy', metrics=['accuracy'])
 		return model
 
-class LSTM():
-	def create_model(self, num_words, max_words = 20, hidden_units = 150):
+class DLSTM():
+	def create_model(self, n_dims = 100, max_words = 20, hidden_units = 150):
 		model = Sequential()
-		model.add(Embedding(num_words, 100, input_length = max_words, trainable = False))
-		model.add(Bidirectional(LSTM(hidden_units)))
+		model.add(LSTM(hidden_units, input_shape = ([n_dims * max_words, 1])))
 		model.add(Dropout(0.5))
 		model.add(Dense(3, activation='softmax'))
 		model.compile('adam', 'categorical_crossentropy', metrics=['accuracy'])
 		return model
+
+if __name__ == "__main__":
+	import pickle as pkl
+	from keras.preprocessing.text import Tokenizer
+	from keras.preprocessing.sequence import pad_sequences
+
